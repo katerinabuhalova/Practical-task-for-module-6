0ffc28df66ee9774039cfc422b21000817f83c6b
Ekaterina <katsiarina.buhalova@gmail.com>
2021-08-02 15:14:36 +0300

    Create project Square. Create class Rectangle.

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        rectangle.squareRectangle(6,8);
+    }
+
+}
---
+++ src/com/company/Rectangle.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Rectangle {
+    private int length;
+    private int width;
+
+
+    public int squareRectangle(int a, int b) {
+        int square = a * b;
+        System.out.println(square);
+        return square;
+    }
+}
